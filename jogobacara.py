f61bd83ccad2909b72fd7869268de10f91de5c3e
LidiaDomingos <lidiaalves2002@outlook.com>
2020-10-17 20:42:28 -0300

    terceiro commit(sem regra avançada:soma jogadores)

--- jogobacara.py
+++ jogobacara.py
@@ -94,7 +94,8 @@ while jogando:
             soma_jogador = soma_jogador % 10 
             
         #condições
-        if soma_jogador <= 5:
+
+        if soma_jogador <= 5 and soma_banco < 8:
             sorteado5 = random.randint(0,51)
             carta_jogador_3 = baralho[sorteado5]
             if baralho[sorteado5] == 'A':
@@ -109,7 +110,7 @@ while jogando:
                 carta_jogador_3 = 0
             soma_jogador = carta_jogador_1 + carta_jogador_2 + carta_jogador_3
         
-        if soma_banco <= 5:
+        if soma_banco <= 5 and soma_jogador < 8:
             sorteado6 = random.randint(0,51)
             carta_banco_3 = baralho[sorteado6]
             if baralho[sorteado6] == 'A':
@@ -155,24 +156,24 @@ while jogando:
                 quantidade_de_fichas -= aposta
                 print('Você tem {0} fichas!'.format(quantidade_de_fichas))
 '''
-#O jogo com 6 baralhos, ou seja, 312 cartas.
+#O jogo com 1, 6 ou 8 baralhos.
 import random
 pergunta_quantidade_baralho = int(input('Com quantos baralhos você deseja jogar?[1, 6 ou 8?]: '))
 baralho = pergunta_quantidade_baralho*['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K','A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K','A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
 quantidade_de_fichas = 100
 jogando = True
 while jogando:
-
-    if quantidade_de_fichas <= 0:
-        print('Você não tem mais fichas para apostar!:C')
-        break
-       
-    aposta = int(input('Qual a quantidade de fichas que você aposta?: '))
-    
-    if aposta == 0:
-        print('Volte sempre! :D')
-        break
+    #com um baralho
     if pergunta_quantidade_baralho == 1:
+        if quantidade_de_fichas <= 0:
+            print('Você não tem mais fichas para apostar!:C')
+            break
+
+        aposta = int(input('Qual a quantidade de fichas que você aposta?: '))
+        
+        if aposta == 0:
+            print('Volte sempre! :D')
+            break
         if quantidade_de_fichas > 0:
             pergunta_ganhador = input('Quem você acha que ganha?[j,b,e]: ')
             #carta um do jogador
@@ -247,7 +248,7 @@ while jogando:
                 soma_jogador = soma_jogador % 10 
                 
             #condições
-            if soma_jogador <= 5:
+            if soma_jogador <= 5 and soma_banco < 8:
                 sorteado5 = random.randint(0,51)
                 carta_jogador_3 = baralho[sorteado5]
                 if baralho[sorteado5] == 'A':
@@ -262,7 +263,7 @@ while jogando:
                     carta_jogador_3 = 0
                 soma_jogador = carta_jogador_1 + carta_jogador_2 + carta_jogador_3
             
-            if soma_banco <= 5:
+            if soma_banco <= 5 and soma_jogador < 8:
                 sorteado6 = random.randint(0,51)
                 carta_banco_3 = baralho[sorteado6]
                 if baralho[sorteado6] == 'A':
@@ -288,21 +289,313 @@ while jogando:
             
             if soma_jogador > soma_banco:
                 if pergunta_ganhador == 'j':
-                    quantidade_de_fichas = quantidade_de_fichas + aposta - 0.0129*aposta
+                    quantidade_de_fichas = quantidade_de_fichas + aposta - int(0.0129*aposta)
                     print('Você tem {0} fichas!'.format(quantidade_de_fichas))
                 else:
                     quantidade_de_fichas -= aposta
                     print('Você tem {0} fichas!'.format(quantidade_de_fichas))
             if soma_jogador < soma_banco:
                 if pergunta_ganhador == 'b':
-                    quantidade_de_fichas = quantidade_de_fichas + int(quantidade_de_fichas*0.95) - 0.0101*aposta
+                    quantidade_de_fichas = quantidade_de_fichas + int(quantidade_de_fichas*0.95) - int(0.0101*aposta)
                     print('Você tem {0} fichas!'.format(quantidade_de_fichas))
                 else:
                     quantidade_de_fichas -= aposta
                     print('Você tem {0} fichas!'.format(quantidade_de_fichas))
             if soma_jogador == soma_banco:
                 if pergunta_ganhador == 'e':
-                    quantidade_de_fichas = quantidade_de_fichas + quantidade_de_fichas*8 - 0.1575*aposta
+                    quantidade_de_fichas = quantidade_de_fichas + quantidade_de_fichas*8 - int(0.1575*aposta)
+                    print('Você tem {0} fichas!'.format(quantidade_de_fichas))
+                else:
+                    quantidade_de_fichas -= aposta
+                    print('Você tem {0} fichas!'.format(quantidade_de_fichas))
+    
+    #com 6 baralhos.
+    if pergunta_quantidade_baralho == 6:
+        if quantidade_de_fichas <= 0:
+            print('Você não tem mais fichas para apostar!:C')
+            break
+
+        aposta = int(input('Qual a quantidade de fichas que você aposta?: '))
+        
+        if aposta == 0:
+            print('Volte sempre! :D')
+            break
+        if quantidade_de_fichas > 0:
+            pergunta_ganhador = input('Quem você acha que ganha?[j,b,e]: ')
+            #carta um do jogador
+            sorteado1 = random.randint(0, 311)
+            if baralho[sorteado1] == 'A':
+                carta_jogador_1 = 1
+            elif baralho[sorteado1] == 'J':
+                carta_jogador_1 = 0
+            elif baralho[sorteado1] == 'Q':
+                carta_jogador_1 = 0
+            elif baralho[sorteado1] == 'K':
+                carta_jogador_1 = 0
+            elif baralho[sorteado1] == 10:
+                carta_jogador_1 = 0
+            else: 
+                carta_jogador_1 = baralho[sorteado1]
+            #carta dois do jogador
+            sorteado2 = random.randint(0,311)
+            if baralho[sorteado2] == 'A':
+                carta_jogador_2 = 1
+            elif baralho[sorteado2] == 'J':
+                carta_jogador_2 = 0
+            elif baralho[sorteado2] == 'Q':
+                carta_jogador_2 = 0
+            elif baralho[sorteado2] == 'K':
+                carta_jogador_2 = 0
+            elif baralho[sorteado2] == 10:
+                carta_jogador_2 = 0
+            else:
+                carta_jogador_2 = baralho[sorteado2]
+
+            #soma das cartas 1 e 2 do jogador
+            soma_jogador = carta_jogador_1 + carta_jogador_2
+
+            #carta um do banco
+            sorteado3 = random.randint(0,311)
+            if baralho[sorteado3] == 'A':
+                carta_banco_1 = 1
+            elif baralho[sorteado3] == 'J':
+                carta_banco_1 = 0
+            elif baralho[sorteado3] == 'Q':
+                carta_banco_1 = 0
+            elif baralho[sorteado3] == 'K':
+                carta_banco_1 = 0
+            elif baralho[sorteado3] == 10:
+                carta_banco_1 = 0
+            else:
+                carta_banco_1 = baralho[sorteado3]
+
+            #carta dois do banco
+            sorteado4 = random.randint(0,311)
+            if baralho[sorteado4] == 'A':
+                carta_banco_2 = 1
+            elif baralho[sorteado4] == 'J':
+                carta_banco_2 = 0
+            elif baralho[sorteado4] == 'Q':
+                carta_banco_2 = 0
+            elif baralho[sorteado4] == 'K':
+                carta_banco_2 = 0
+            elif baralho[sorteado4] == 10:
+                carta_banco_2 = 0
+            else: 
+                carta_banco_2 = baralho[sorteado4]
+
+            #soma das cartas dos banco
+            soma_banco = carta_banco_1 + carta_banco_2
+
+            if soma_banco >= 10:
+                soma_banco = soma_banco % 10    
+            
+            if soma_jogador >= 10:
+                soma_jogador = soma_jogador % 10 
+                
+            #condições
+            if soma_jogador <= 5 and soma_banco < 8:
+                sorteado5 = random.randint(0,311)
+                carta_jogador_3 = baralho[sorteado5]
+                if baralho[sorteado5] == 'A':
+                    carta_jogador_3 = 1
+                elif baralho[sorteado5] == 'J':
+                    carta_jogador_3 = 0
+                elif baralho[sorteado5] == 'Q':
+                    carta_jogador_3 = 0
+                elif baralho[sorteado5] == 'K':
+                    carta_jogador_3 = 0
+                elif baralho[sorteado5] == 10:
+                    carta_jogador_3 = 0
+                soma_jogador = carta_jogador_1 + carta_jogador_2 + carta_jogador_3
+            
+            if soma_banco <= 5 and soma_jogador < 8:
+                sorteado6 = random.randint(0,311)
+                carta_banco_3 = baralho[sorteado6]
+                if baralho[sorteado6] == 'A':
+                    carta_banco_3 = 1
+                elif baralho[sorteado6] == 'J':
+                    carta_banco_3 = 0
+                elif baralho[sorteado6] == 'Q':
+                    carta_banco_3 = 0
+                elif baralho[sorteado6] == 'K':
+                    carta_banco_3 = 0
+                elif baralho[sorteado6] == 10:
+                    carta_banco_3 = 0
+                soma_banco = carta_banco_1 + carta_banco_2 + carta_banco_3
+
+            if soma_banco >= 10:
+                soma_banco = soma_banco % 10    
+            
+            if soma_jogador >= 10:
+                soma_jogador = soma_jogador % 10  
+
+            print('A soma das cartas do jogador deu {0}'.format(soma_jogador))
+            print('A soma das cartas do banco deu {0}'.format(soma_banco))
+            
+            if soma_jogador > soma_banco:
+                if pergunta_ganhador == 'j':
+                    quantidade_de_fichas = quantidade_de_fichas + aposta - int(0.0124*aposta)
+                    print('Você tem {0} fichas!'.format(quantidade_de_fichas))
+                else:
+                    quantidade_de_fichas -= aposta
+                    print('Você tem {0} fichas!'.format(quantidade_de_fichas))
+            if soma_jogador < soma_banco:
+                if pergunta_ganhador == 'b':
+                    quantidade_de_fichas = quantidade_de_fichas + int(quantidade_de_fichas*0.95) - int(0.0106*aposta)
+                    print('Você tem {0} fichas!'.format(quantidade_de_fichas))
+                else:
+                    quantidade_de_fichas -= aposta
+                    print('Você tem {0} fichas!'.format(quantidade_de_fichas))
+            if soma_jogador == soma_banco:
+                if pergunta_ganhador == 'e':
+                    quantidade_de_fichas = quantidade_de_fichas + quantidade_de_fichas*8 - int(0.1444*aposta)
+                    print('Você tem {0} fichas!'.format(quantidade_de_fichas))
+                else:
+                    quantidade_de_fichas -= aposta
+                    print('Você tem {0} fichas!'.format(quantidade_de_fichas))
+    
+    #com 8 baralhos.
+    if pergunta_quantidade_baralho == 8:
+        if quantidade_de_fichas <= 0:
+            print('Você não tem mais fichas para apostar!:C')
+            break
+
+        aposta = int(input('Qual a quantidade de fichas que você aposta?: '))
+        
+        if aposta == 0:
+            print('Volte sempre! :D')
+            break
+        if quantidade_de_fichas > 0:
+            pergunta_ganhador = input('Quem você acha que ganha?[j,b,e]: ')
+            #carta um do jogador
+            sorteado1 = random.randint(0, 415)
+            if baralho[sorteado1] == 'A':
+                carta_jogador_1 = 1
+            elif baralho[sorteado1] == 'J':
+                carta_jogador_1 = 0
+            elif baralho[sorteado1] == 'Q':
+                carta_jogador_1 = 0
+            elif baralho[sorteado1] == 'K':
+                carta_jogador_1 = 0
+            elif baralho[sorteado1] == 10:
+                carta_jogador_1 = 0
+            else: 
+                carta_jogador_1 = baralho[sorteado1]
+            #carta dois do jogador
+            sorteado2 = random.randint(0,415)
+            if baralho[sorteado2] == 'A':
+                carta_jogador_2 = 1
+            elif baralho[sorteado2] == 'J':
+                carta_jogador_2 = 0
+            elif baralho[sorteado2] == 'Q':
+                carta_jogador_2 = 0
+            elif baralho[sorteado2] == 'K':
+                carta_jogador_2 = 0
+            elif baralho[sorteado2] == 10:
+                carta_jogador_2 = 0
+            else:
+                carta_jogador_2 = baralho[sorteado2]
+
+            #soma das cartas 1 e 2 do jogador
+            soma_jogador = carta_jogador_1 + carta_jogador_2
+
+            #carta um do banco
+            sorteado3 = random.randint(0,415)
+            if baralho[sorteado3] == 'A':
+                carta_banco_1 = 1
+            elif baralho[sorteado3] == 'J':
+                carta_banco_1 = 0
+            elif baralho[sorteado3] == 'Q':
+                carta_banco_1 = 0
+            elif baralho[sorteado3] == 'K':
+                carta_banco_1 = 0
+            elif baralho[sorteado3] == 10:
+                carta_banco_1 = 0
+            else:
+                carta_banco_1 = baralho[sorteado3]
+
+            #carta dois do banco
+            sorteado4 = random.randint(0,415)
+            if baralho[sorteado4] == 'A':
+                carta_banco_2 = 1
+            elif baralho[sorteado4] == 'J':
+                carta_banco_2 = 0
+            elif baralho[sorteado4] == 'Q':
+                carta_banco_2 = 0
+            elif baralho[sorteado4] == 'K':
+                carta_banco_2 = 0
+            elif baralho[sorteado4] == 10:
+                carta_banco_2 = 0
+            else: 
+                carta_banco_2 = baralho[sorteado4]
+
+            #soma das cartas dos banco
+            soma_banco = carta_banco_1 + carta_banco_2
+
+            if soma_banco >= 10:
+                soma_banco = soma_banco % 10    
+            
+            if soma_jogador >= 10:
+                soma_jogador = soma_jogador % 10 
+                
+            #condições
+            if soma_jogador <= 5 and soma_banco < 8:
+                sorteado5 = random.randint(0,415)
+                carta_jogador_3 = baralho[sorteado5]
+                if baralho[sorteado5] == 'A':
+                    carta_jogador_3 = 1
+                elif baralho[sorteado5] == 'J':
+                    carta_jogador_3 = 0
+                elif baralho[sorteado5] == 'Q':
+                    carta_jogador_3 = 0
+                elif baralho[sorteado5] == 'K':
+                    carta_jogador_3 = 0
+                elif baralho[sorteado5] == 10:
+                    carta_jogador_3 = 0
+                soma_jogador = carta_jogador_1 + carta_jogador_2 + carta_jogador_3
+            
+            if soma_banco <= 5 and soma_jogador < 8:
+                sorteado6 = random.randint(0,415)
+                carta_banco_3 = baralho[sorteado6]
+                if baralho[sorteado6] == 'A':
+                    carta_banco_3 = 1
+                elif baralho[sorteado6] == 'J':
+                    carta_banco_3 = 0
+                elif baralho[sorteado6] == 'Q':
+                    carta_banco_3 = 0
+                elif baralho[sorteado6] == 'K':
+                    carta_banco_3 = 0
+                elif baralho[sorteado6] == 10:
+                    carta_banco_3 = 0
+                soma_banco = carta_banco_1 + carta_banco_2 + carta_banco_3
+
+            if soma_banco >= 10:
+                soma_banco = soma_banco % 10    
+            
+            if soma_jogador >= 10:
+                soma_jogador = soma_jogador % 10  
+
+            print('A soma das cartas do jogador deu {0}'.format(soma_jogador))
+            print('A soma das cartas do banco deu {0}'.format(soma_banco))
+            
+            if soma_jogador > soma_banco:
+                if pergunta_ganhador == 'j':
+                    quantidade_de_fichas = quantidade_de_fichas + aposta - int(0.0124*aposta)
+                    print('Você tem {0} fichas!'.format(quantidade_de_fichas))
+                else:
+                    quantidade_de_fichas -= aposta
+                    print('Você tem {0} fichas!'.format(quantidade_de_fichas))
+            if soma_jogador < soma_banco:
+                if pergunta_ganhador == 'b':
+                    quantidade_de_fichas = quantidade_de_fichas + int(quantidade_de_fichas*0.95) - int(0.0106*aposta)
+                    print('Você tem {0} fichas!'.format(quantidade_de_fichas))
+                else:
+                    quantidade_de_fichas -= aposta
+                    print('Você tem {0} fichas!'.format(quantidade_de_fichas))
+            if soma_jogador == soma_banco:
+                if pergunta_ganhador == 'e':
+                    quantidade_de_fichas = quantidade_de_fichas + quantidade_de_fichas*8 - int(0.1436*aposta)
                     print('Você tem {0} fichas!'.format(quantidade_de_fichas))
                 else:
                     quantidade_de_fichas -= aposta
